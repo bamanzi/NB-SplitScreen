5020ee7e3513bf09b13933ac3e8ff3e4fa7fb6f2
Adam Kubista <kubistaa@gmail.com>
2016-01-08 12:31:09 +0100

    removed old code

--- src/eu/fidli/tabgroupswitch/GoToNextTabGroup.java
+++ src/eu/fidli/tabgroupswitch/GoToNextTabGroup.java
@@ -35,45 +35,6 @@ public final class GoToNextTabGroup extends TabGroups implements ActionListener 
     
     @Override
     public void actionPerformed(ActionEvent e) {
-	/*WindowManager manager =	WindowManager.getDefault();
-	TopComponent active = TopComponent.getRegistry().getActivated();
-	
-	Set<Mode> modes = (Set<Mode>)manager.getModes();
-	boolean seenActive = false;
-	boolean firstOccurence = false;
-	TopComponent first = null;
-	boolean changed = false;
-	for(Mode m : modes){
-		    TopComponent top = m.getSelectedTopComponent();
-		    
-		    if(top != null){
-			String className = top.getClass().getName();
-			if(className.equals("org.netbeans.core.multiview.MultiViewCloneableTopComponent")){
-			    if(!firstOccurence){
-				firstOccurence = true;
-				first = top;
-			    }
-			    if(top == active){
-				seenActive = true;
-				continue;
-			    }
-			    if(!seenActive){
-				continue;
-			    }
-			    if(active != top){
-				top.requestActive();
-				changed = true;
-				break;
-			    }
-			}
-			
-		    }
-	}
-	
-	if(!changed && first != null){
-	    first.requestActive();
-	}
-	*/
 	Mode nextTabGroup = getNextDocumentTabGroup();
 	if(nextTabGroup != null){
 	    nextTabGroup.getSelectedTopComponent().requestActive();
